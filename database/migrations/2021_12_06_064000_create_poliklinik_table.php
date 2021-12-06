545cf4a28aac404a4f1ca026f0d8fde10ef54699
Hermawan <agenghermawan16@gmail.com>
2021-12-06 15:31:17 +0700

    create new fitur poliklinik

---
+++ database/migrations/2021_12_06_064000_create_poliklinik_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePoliklinikTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('poliklinik', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama',40);
+            $table->string('deskripsi',150);
+            $table->json('tindakanmedis');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('poliklinik');
+    }
+}
